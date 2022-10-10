f6780aaa3e79526da48b4f13b58d5a017423abe6
MohamedElarbawy <arbawym@gmail.com>
2022-10-10 15:23:41 +0200

    binary search

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+using Algorithms.Search;
+
+Console.WriteLine("Hello, World!");
+
+
+int[] arr = { 2, 3, 4, 5, 9, 10, 13, 14, 17, 18, 20 ,21};
+
+
+
+Console.WriteLine(BinarySearch.Search(arr,18)); 
---
+++ Search/BinarySearch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Search
+{
+    public static class BinarySearch
+    {
+
+        //To Use This Function You Have To Pass A Sorted Array
+        public static int Search(int[] SortedArray, int SearchedItem)
+        {
+            int IndexOfSearchedItem = -1; //It Means The Searched Item Is Not Existed In The Array
+            int high = SortedArray.Length - 1;
+            int low = 0;
+            while (high >= low)
+            {
+                int mid = (int)((high + low) / 2);
+                if (SearchedItem == SortedArray[mid])
+                {
+                    IndexOfSearchedItem = mid;
+                    return IndexOfSearchedItem;
+                }
+                if (SearchedItem > SortedArray[mid])
+                    low = mid + 1;
+                else if (SearchedItem < SortedArray[mid])
+                    high = mid - 1;
+            }
+            return IndexOfSearchedItem;
+        }
+    }
+}
